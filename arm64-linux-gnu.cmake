69309f4578e07dfaee5e1f593d22d6146910304f
Федосеев Антон Ильич <fedoseevai@sibur.ru>
2023-01-13 17:21:25 +0300

    Сборка для arm64

---
+++ arm64-linux-gnu.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm64)
+
+set(CMAKE_SYSROOT /media/storage/work/smart-chemodan/streamer/webrtc/src/build/linux/debian_bullseye_arm64-sysroot)
+set(tools /media/storage/work/smart-chemodan/streamer/webrtc/src/third_party/llvm-build/Release+Asserts)
+
+set(triple arm64-linux-gnu)
+set(CMAKE_C_COMPILER ${tools}/bin/clang)
+set(CMAKE_C_COMPILER_TARGET ${triple})
+set(CMAKE_CXX_COMPILER ${tools}/bin/clang++)
+set(CMAKE_CXX_COMPILER_TARGET ${triple})
+add_link_options("-fuse-ld=lld")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
